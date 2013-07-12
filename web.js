d24324e1ebbc9d6db9b981b9a8b5e04cfb681e5e
sunilgopinath <sxg282@gmail.com>
2013-07-12 03:25:35 +0000

    experimenting with fs.readFileSync

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var html = fs.readFileSync('index.html',"utf8");
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
